4344e1a0a3bf95f8b715b8b3121f33f74a60a892
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-06-11 21:07:08 -0300

    Criando repositório e projeto react

---
+++ hello-react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello React! 👾</h1>
+  );
+}
+
+export default App;
---
+++ hello-react/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <h1>Hello React! 👽</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
